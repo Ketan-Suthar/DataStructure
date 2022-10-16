9845049e655a0aa75dae2413877d51b8a29da86a
anandkumar08120 <79368978+anandkumar08120@users.noreply.github.com>
2022-10-17 04:05:24 +0530

    merge_K_sorted_list

---
+++ C++/Heap/merge_K_sorted_list.cpp
@@ -0,0 +1,67 @@
+// You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+// Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+// Example 1:
+
+// Input: lists = [[1,4,5],[1,3,4],[2,6]]
+// Output: [1,1,2,3,4,4,5,6]
+// Explanation: The linked-lists are:
+// [
+//   1->4->5,
+//   1->3->4,
+//   2->6
+// ]
+// merging them into one sorted list:
+// 1->1->2->3->4->4->5->6
+// Example 2:
+
+// Input: lists = []
+// Output: []
+// Example 3:
+
+// Input: lists = [[]]
+// Output: []
+ 
+
+// Constraints:
+
+// k == lists.length
+// 0 <= k <= 104
+// 0 <= lists[i].length <= 500
+// -104 <= lists[i][j] <= 104
+// lists[i] is sorted in ascending order.
+// The sum of lists[i].length will not exceed 104.
+
+
+
+//CODE
+ListNode *mergeKLists(vector<ListNode *> &lists) {
+    if(lists.empty()){
+        return nullptr;
+    }
+    while(lists.size() > 1){
+        lists.push_back(mergeTwoLists(lists[0], lists[1]));
+        lists.erase(lists.begin());
+        lists.erase(lists.begin());
+    }
+    return lists.front();
+}
+ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+    if(l1 == nullptr){
+        return l2;
+    }
+    if(l2 == nullptr){
+        return l1;
+    }
+    if(l1->val <= l2->val){
+        l1->next = mergeTwoLists(l1->next, l2);
+        return l1;
+    }
+    else{
+        l2->next = mergeTwoLists(l1, l2->next);
+        return l2;
+    }
+}
